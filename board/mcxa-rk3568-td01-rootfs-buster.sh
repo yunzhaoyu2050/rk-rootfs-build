9d9cf4247a43e9fc6ca5807726c13866976de008
yunzhaoyu <yunzhaoyu@outlook.com>
2022-03-22 18:07:13 +0800

    update software arch

---
+++ board/mcxa-rk3568-td01-rootfs-buster.sh
@@ -0,0 +1,190 @@
+#!/bin/bash -e
+
+source "${PRDIR}/"script/function.sh
+
+# check board name
+[ "$BOARD_NAME" != "mcxa-rk3568-td01" ] &&
+  error "board name($BOARD_NAME) is not mcxa-rk3568-td01, please check" &&
+  return 1
+
+# Directory contains the target rootfs
+TARGET_ROOTFS_DIR="binary"
+
+case "${ARCH:-$1}" in
+arm | arm32 | armhf)
+  ARCH=armhf
+  ;;
+*)
+  ARCH=arm64
+  ;;
+esac
+
+echo -e "\033[36m Building for $ARCH \033[0m"
+
+if [ ! $VERSION ]; then
+  VERSION="release"
+fi
+
+if [ ! -e linaro-buster-alip-*.tar.gz ]; then
+  echo -e "\033[36m Run mk-base-debian.sh first \033[0m"
+  exit -1
+fi
+
+finish() {
+  sudo umount $TARGET_ROOTFS_DIR/dev
+  exit -1
+}
+trap finish ERR
+
+echo -e "\033[36m Extract image \033[0m"
+sudo tar -xpf linaro-buster-alip-*.tar.gz
+
+# packages folder
+sudo mkdir -p $TARGET_ROOTFS_DIR/packages
+sudo cp -rf packages/$ARCH/* $TARGET_ROOTFS_DIR/packages
+
+# overlay folder
+sudo cp -rf "${PRDIR}/overlay-bd/$BOARD_NAME/"overlay/* $TARGET_ROOTFS_DIR/
+
+# overlay-firmware folder
+sudo cp -rf "${PRDIR}/overlay-bd/$BOARD_NAME/"overlay-firmware/* $TARGET_ROOTFS_DIR/
+
+# overlay-debug folder
+# adb, video, camera  test file
+if [ "$VERSION" == "debug" ]; then
+  sudo cp -rf "${PRDIR}/overlay-bd/$BOARD_NAME/"overlay-debug/* $TARGET_ROOTFS_DIR/
+fi
+
+# # adb
+# if [[ "$ARCH" == "armhf" && "$VERSION" == "debug" ]]; then
+#   sudo cp -f "${PRDIR}/overlay-bd/$BOARD_NAME/"overlay-debug/usr/local/share/adb/adbd-32 $TARGET_ROOTFS_DIR/usr/bin/adbd
+# elif [[ "$ARCH" == "arm64" && "$VERSION" == "debug" ]]; then
+#   sudo cp -f "${PRDIR}/overlay-bd/$BOARD_NAME/"overlay-debug/usr/local/share/adb/adbd-64 $TARGET_ROOTFS_DIR/usr/bin/adbd
+# fi
+
+# bt/wifi firmware
+sudo mkdir -p $TARGET_ROOTFS_DIR/system/lib/modules/
+sudo mkdir -p $TARGET_ROOTFS_DIR/vendor/etc
+[ ! $KERNEL_PATH ] && KERNEL_PATH="../kernel" # default
+sudo find "${KERNEL_PATH}"/drivers/net/wireless/rockchip_wlan/* -name "*.ko" |
+  xargs -n1 -i sudo cp {} $TARGET_ROOTFS_DIR/system/lib/modules/
+
+echo -e "\033[36m Change root.....................\033[0m"
+if [ "$ARCH" == "armhf" ]; then
+  sudo cp /usr/bin/qemu-arm-static $TARGET_ROOTFS_DIR/usr/bin/
+elif [ "$ARCH" == "arm64" ]; then
+  sudo cp /usr/bin/qemu-aarch64-static $TARGET_ROOTFS_DIR/usr/bin/
+fi
+sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
+
+cat <<EOF | sudo chroot $TARGET_ROOTFS_DIR
+# apt-get update
+# apt-get upgrade -y
+
+chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+chmod +x /etc/rc.local
+
+export APT_INSTALL="apt-get install -fy --allow-downgrades"
+# export APT_INSTALL="apt-get install -fy"
+
+#---------------power management --------------
+\${APT_INSTALL} pm-utils triggerhappy
+cp /etc/Powermanager/triggerhappy.service  /lib/systemd/system/triggerhappy.service
+
+# #---------------Rga--------------
+# \${APT_INSTALL} /packages/rga/*.deb
+
+# echo -e "\033[36m Setup Video.................... \033[0m"
+# \${APT_INSTALL} gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-alsa \
+#   gstreamer1.0-plugins-base-apps qtmultimedia5-examples
+
+# \${APT_INSTALL} /packages/mpp/*
+# \${APT_INSTALL} /packages/gst-rkmpp/*.deb
+# \${APT_INSTALL} /packages/gstreamer/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-base1.0/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-bad1.0/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-good1.0/*.deb
+
+# #---------Camera---------
+# echo -e "\033[36m Install camera.................... \033[0m"
+# \${APT_INSTALL} cheese v4l-utils
+# \${APT_INSTALL} /packages/rkisp/*.deb
+# \${APT_INSTALL} /packages/rkaiq/*.deb
+# \${APT_INSTALL} /packages/libv4l/*.deb
+
+# #---------Xserver---------
+# echo -e "\033[36m Install Xserver.................... \033[0m"
+# \${APT_INSTALL} /packages/xserver/*.deb
+
+#---------------Openbox--------------
+echo -e "\033[36m Install openbox.................... \033[0m"
+# \${APT_INSTALL} /packages/openbox/*.deb
+# \${APT_INSTALL} openbox
+
+# #---------update chromium-----
+# \${APT_INSTALL} /packages/chromium/*.deb
+
+#------------------libdrm------------
+echo -e "\033[36m Install libdrm.................... \033[0m"
+\${APT_INSTALL} /packages/libdrm/*.deb
+
+# #------------------libdrm-cursor------------
+# echo -e "\033[36m Install libdrm-cursor.................... \033[0m"
+# \${APT_INSTALL} /packages/libdrm-cursor/*.deb
+
+# # Only preload libdrm-cursor for X
+# sed -i "/libdrm-cursor.so/d" /etc/ld.so.preload
+# sed -i "1aexport LD_PRELOAD=libdrm-cursor.so.1" /usr/bin/X
+
+# #------------------pcmanfm------------
+# echo -e "\033[36m Install pcmanfm.................... \033[0m"
+# \${APT_INSTALL} /packages/pcmanfm/*.deb
+
+#------------------rkwifibt------------
+echo -e "\033[36m Install rkwifibt.................... \033[0m"
+\${APT_INSTALL} /packages/rkwifibt/*.deb
+ln -s /system/etc/firmware /vendor/etc/
+
+# if [ "$VERSION" == "debug" ]; then
+# #------------------glmark2------------
+# echo -e "\033[36m Install glmark2.................... \033[0m"
+# \${APT_INSTALL} /packages/glmark2/*.deb
+# fi
+
+# echo -e "\033[36m Install synaptic/onboard.................... \033[0m"
+# \${APT_INSTALL} synaptic onboard
+
+# echo -e "\033[36m Install network vpn.................... \033[0m"
+# \${APT_INSTALL} network-manager-l2tp network-manager-openvpn network-manager-pptp network-manager-strongswan network-manager-vpnc
+# apt install -fy network-manager-gnome --reinstall
+
+# #------------------pulseaudio---------
+# echo -e "\033[36m Install pulseaudio................. \033[0m"
+# no|apt-get install -fy --allow-downgrades /packages/pulseaudio/*.deb
+
+# mark package to hold
+apt list --installed | grep -v oldstable | cut -d/ -f1 | xargs apt-mark hold
+
+#---------------Custom Script--------------
+systemctl mask systemd-networkd-wait-online.service
+systemctl mask NetworkManager-wait-online.service
+rm /lib/systemd/system/wpa_supplicant@.service
+
+#systemctl disable hostapd.service
+mv /lib/systemd/system/hostapd.service /lib/systemd/system/hostapd.service.bk
+systemctl enable adbd.service
+systemctl enable async.service
+systemctl enable resize-disk.service
+systemctl enable rkwifibt.service
+systemctl enable rockchip.service
+systemctl enable chsup.service
+
+#---------------Clean--------------
+rm -rf /var/lib/apt/lists/*
+rm -rf /packages/chromium /packages/ffmpeg /packages/glmark2 /packages/gst-base /packages/gst-rkmpp \
+  /packages/libdrm /packages/libv4l /packages/mpp /packages/mpv /packages/openbox /packages/pcmanfm \
+  /packages/pulseaudio /packages/rga /packages/rkisp /packages/rkwifibt /packages/xserver
+
+EOF
+
+sudo umount $TARGET_ROOTFS_DIR/dev
--- readme.md
+++ readme.md
@@ -1,92 +1,23 @@
-fork from https://github.com/rockchip-linux/rk-rootfs-build.git
+### rk-rootfs-build
 
-note: 1、以chromium应用自启动，删除原始桌面启动（测试阶段）
-
-## Introduction
-
-A set of shell scripts that will build GNU/Linux distribution rootfs image
-for rockchip platform.
-
-## Available Distro
-
-* Debian 10 (Buster-X11 and Wayland)~~
-
-```
-sudo apt-get install binfmt-support qemu-user-static
-sudo dpkg -i ubuntu-build-service/packages/*
-sudo apt-get install -f
-```
-
-## Usage for 32bit Debian 10 (Buster-32)
-
-Building a base debian system by ubuntu-build-service from linaro.
-
-```
-	RELEASE=buster TARGET=desktop ARCH=armhf ./mk-base-debian.sh
+#### Directory Description
+```bash
+board/  -- user define rootfs build cmd folder, format:'board_name'-rootfs-buster.sh
+    mk-rootfs-buster.sh  -- default build cmd
+    mcxa-rk3568-td01-rootfs-buster.sh  -- 'mcxa-rk3568-td01' board build cmd
+overlay-bd/
+    default/
+    mcxa-rk3568-td01/  -- 'mcxa-rk3568-td01' board user define rootfs files
+        overlay/
+        overlay-debug/  -- debug files
+        overlay-firmware/
+script/
+build.sh
+rootfs.cfg
+setenv.sh
 ```
 
-Building the rk-debian rootfs:
+#### Official repository mirror
 
-```
-	RELEASE=buster ARCH=armhf ./mk-rootfs.sh
-```
-
-Building the rk-debain rootfs with debug:
-
-```
-	VERSION=debug ARCH=armhf ./mk-rootfs-buster.sh
-```
-
-Creating the ext4 image(linaro-rootfs.img):
-
-```
-	./mk-image.sh
-```
-
----
-
-## Usage for 64bit Debian 10 (Buster-64)
-
-Building a base debian system by ubuntu-build-service from linaro.
-
-```
-	RELEASE=buster TARGET=desktop ARCH=arm64 ./mk-base-debian.sh
-```
-
-Building the rk-debian rootfs:
-
-```
-	RELEASE=buster ARCH=arm64 ./mk-rootfs.sh
-```
-
-Building the rk-debain rootfs with debug:
-
-```
-	VERSION=debug ARCH=arm64 ./mk-rootfs-buster.sh
-```
-
-Creating the ext4 image(linaro-rootfs.img):
-
-```
-	./mk-image.sh
-```
----
-
-## Cross Compile for ARM Debian
-
-[Docker + Multiarch](http://opensource.rock-chips.com/wiki_Cross_Compile#Docker)
-
-## Package Code Base
-
-Please apply [those patches](https://github.com/rockchip-linux/rk-rootfs-build/tree/master/packages-patches) to release code base before rebuilding!
-
-## FAQ
-
-- noexec or nodev issue
-noexec or nodev issue /usr/share/debootstrap/functions: line 1450:
-../rootfs/ubuntu-build-service/stretch-desktop-arm64/chroot/test-dev-null:
-Permission denied E: Cannot install into target
-...
-mounted with noexec or nodev
-
-Solution: mount -o remount,exec,dev xxx (xxx is the mount place), then rebuild it.
+address: https://github.com/JeffyCN/rockchip_mirrors.git
+branch: debian
